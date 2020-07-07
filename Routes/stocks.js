82c47d28e5df0dd084296330dba6b0db8dffcd61
Kartik Goel <goel.kartik39@gmail.com>
2020-07-07 12:25:21 +0530

    fixed update route

--- Routes/stocks.js
+++ Routes/stocks.js
@@ -25,17 +25,43 @@ router.post("/create",requireLogin,(req,res) => {
 //Updating the stockData using the particular stock id
 // POST /stocks/update
 // @Protected
-router.post("/update",requireLogin,(req,res) =>{
-    User.findByIdAndUpdate(req.user.id)
-    .then(() => {
-        User.update({'stockData._id': req.body._id}, {'$set': {
-            'stockData.$.name': req.body.name,
-            'stockData.$.quantity': req.body.quantity,
-            'stockData.$.price': req.body.price
-        }}).then((user) => {
-            res.status(200).json(user);
-        }).catch((err) => {
-            res.status(422).json(err);
+// router.post("/update",requireLogin,(req,res) =>{
+//     User.findByIdAndUpdate(req.user.id)
+//     .then(() => {
+//         User.update({"stockData._id": req.body._id}, {$set: {
+//             'stockData.$.name': req.body.name,
+//             'stockData.$.quantity': req.body.quantity,
+//             'stockData.$.price': req.body.price
+//         }}).then((user) => {
+//             res.status(200).json(user);
+//         }).catch((err) => {
+//             res.status(422).json(err);
+//         })
+//     }).catch((err) => {
+//         res.json(err);
+//     })
+// })
+
+router.post("/update",requireLogin,(req,res) => {
+    const {_id,name,quantity,price} = req.body;
+
+    const stockData = {
+        _id,
+        name,
+        quantity,
+        price
+    }
+
+    User.findById(req.user._id)
+    .then((user) => {
+        user.stockData = user.StockData.filter((stock) => {
+            stock._id !== req.body._id 
+        })
+        console.log(user,"hello");
+        user.stockData = stockData;
+        user.save()
+        .then((user) => {
+            res.json(user);
         })
     }).catch((err) => {
         res.json(err);
